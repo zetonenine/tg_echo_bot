4633e11ec398ee06440d03fe5137fe9ccdbd1d66
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-06-27 21:43:48 +0300

    Clean code, check for removing container

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ import os
 from aiogram import Bot, Dispatcher, executor, types
 # from postgresql import BD
 
-print(os.environ.get('TOKEN'))
-
 API_TOKEN = os.environ.get('TOKEN')
-
-
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -19,36 +15,12 @@ dp = Dispatcher(bot)
 # db = BD()
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
-
-
-@dp.message_handler(regexp='(^cat[s]?$|puss)')
-async def cats(message: types.Message):
-    with open('data/cats.jpg', 'rb') as photo:
-        '''
-        # Old fashioned way:
-        await bot.send_photo(
-            message.chat.id,
-            photo,
-            caption='Cats are here 😺',
-            reply_to_message_id=message.message_id,
-        )
-        '''
-
-        await message.reply_photo(photo, caption='Cats are here 😺')
-
-
 @dp.message_handler()
 async def echo(message: types.Message):
     # old style:
     # await bot.send_message(message.chat.id, message.text)
     # db.add_word(message.text)
-    await message.answer(f"Нет, ты {message.text}!")
+    await message.answer(f"Нееее, ты {message.text}!")
 
 
 if __name__ == '__main__':
