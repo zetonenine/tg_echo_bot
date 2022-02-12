c9ac5780664c9426d9f71eb69061d1e2ceb120c3
Andrey <66561503+zetonenine@users.noreply.github.com>
2022-02-12 20:13:04 +0300

    Test CI/CD commit 1

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, \
+    ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 from aiogram import Bot, Dispatcher, executor, types
 from postgresql import Database
 
@@ -9,16 +11,26 @@ API_TOKEN = "599026278:AAGtNDoDBN_KRmK5bJ9hTfT4gD7dsazaCig"
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 db = Database()
 
+log.info('Сообщение из main.py')
+
+
+@dp.message_handler(commands=['nuka'])
+async def nuka(message: types.Message):
+    pass
+
 
 @dp.message_handler(commands=['show'])
 async def count_user(message: types.Message):
     result = db.show_words()
+    # result = session.query(Words.text).all()
+    # session.commit()
     await message.answer(result)
 
 
@@ -26,7 +38,10 @@ async def count_user(message: types.Message):
 async def echo(message: types.Message):
     # old style:
     # await bot.send_message(message.chat.id, message.text)
-    db.add_word(message.text)
+    # db.add_word(message.text)
+    # inst = Words(text=message.text)
+    # session.add(inst)
+    # session.commit()
     await message.answer(f"Нееее, ты {message.text}!")
 
 
